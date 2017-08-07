6a73aba45721e0d6c27460e6711093332c2ade22
skyran1278 <skyran1278@gmail.com>
2017-08-07 16:08:11 +0800

    移動檔案

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import BookList from "../containers/book-list";
+// import BookDetail from "../containers/book-detail";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+        {/* <BookDetail /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
