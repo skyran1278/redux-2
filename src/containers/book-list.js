1041576c27ad7e606f9b53af7115092c12092c41
skyran1278 <skyran1278@gmail.com>
2017-08-07 19:13:23 +0800

    actions

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from './../actions';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
@@ -22,4 +25,23 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed
+  // to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
