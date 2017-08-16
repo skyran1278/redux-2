c9a90f4f08295ae39cfd7e33a63020f7e74d4e50
skyran1278 <skyran1278@gmail.com>
2017-08-16 20:25:04 +0800

    完成第 4 節

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import BookList from "../containers/book-list";
-// import BookDetail from "../containers/book-detail";
+import BookDetail from "../containers/book-detail";
 
 class App extends Component {
   render() {
     return (
       <div>
         <BookList />
-        {/* <BookDetail /> */}
+        <BookDetail />
       </div>
     );
   }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { selectBook } from './../actions';
-import { bindActionCreators } from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
 
 class BookList extends Component {
   renderList() {
-    return (
-      this.props.books.map((book) => {
-        return (
-          <li key={book.title} className="list-group-item">{book.title}</li>
-        )
-      })
-    )
+    return this.props.books.map(book => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item"
+        >
+          {book.title}
+        </li>
+      );
+    });
   }
 
   render() {
     return (
-      <div>
-        <ul className="list-group col-sm-4">
-          {this.renderList()}
-        </ul>
-      </div>
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
     );
   }
 }
@@ -36,7 +38,7 @@ function mapStateToProps(state) {
 // Anything returned from this function will end up as props
 // on the BookList container
 function mapDispatchToProps(dispatch) {
-  // Whenever selectBook is called, the result should be passed
+  // Whenever selectBook is called, the result shoudl be passed
   // to all of our reducers
   return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
